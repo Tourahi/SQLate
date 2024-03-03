58547fa2599c51a0d2a35887d0dac7b32e500350
tourahi <amine-fb-fb@hotmail.fr>
2024-03-03 17:27:44 +0100

    Prompt loop.

--- SQLate.c
+++ SQLate.c
@@ -1,3 +1,6 @@
+#define _GNU_SOURCE
+#define DEBUG_ON false
+
 #include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
@@ -17,7 +20,7 @@ typedef struct {
 } InptBuffer;
 
 InptBuffer* newInptBuffer() {
-  logInfo("Creating new input buffer.");
+  if (DEBUG_ON) logInfo("Creating new input buffer.");
   InptBuffer* inptBuffer = (InptBuffer*)malloc(sizeof(InptBuffer));
   inptBuffer->buffer = NULL;
   inptBuffer->bufferLenght = 0;
@@ -25,9 +28,39 @@ InptBuffer* newInptBuffer() {
   return inptBuffer;
 }
 
+void readInpt(InptBuffer* inptBuffer) {
+  ssize_t bytesRead =
+    getline(&(inptBuffer->buffer), &(inptBuffer->bufferLenght), stdin);
+
+  if (bytesRead <= 0) {
+    logError("Error reding input");
+    exit(EXIT_FAILURE);
+  }
+
+  // Ignore trailing newLine
+  inptBuffer->inputLenght = bytesRead - 1;
+  inptBuffer->buffer[bytesRead - 1];
+}
+
+void closeInptBuffer(InptBuffer* InptBuffer) {
+  free(InptBuffer->buffer);
+  free(InptBuffer);
+}
+
+void printPrompt() { printf("db > "); }
 
 int main(int argc, char const *argv[])
 {
-  newInptBuffer();
+  InptBuffer* inptBuffer = newInptBuffer();
+  while (true) {
+    printPrompt();
+    readInpt(inptBuffer);
+    if (strcmp(inptBuffer->buffer, "exit\n") == 0) {
+      closeInptBuffer(inptBuffer);
+      exit(EXIT_SUCCESS);
+    } else {
+      printf("Unrecognized command '%s'.\n", inptBuffer->buffer);
+    }
+  }
   return 0;
 }
